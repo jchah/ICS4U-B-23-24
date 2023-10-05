4d4c9d3a56ea41e90031907fb90749430f2ac560
Jad <98987031+jchah@users.noreply.github.com>
2023-10-05 15:10:16 -0400

    Readjusts image grid on window resize, loads images using javascript, navbar fixes hamburger menu and dropdown, style changed (to be continued)

---
+++ grid-layout/generateImageGrid.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Array of image sources
+    const imageSources = {
+        modern: [
+        ],
+        traditional: [
+        ],
+        apartments: [
+        ],
+    };
+
+    for (let i = 1; i <= 12; i++) {
+        imageSources.modern[i - 1] = "img/modern" + i + ".jpg";
+        imageSources.traditional[i - 1] = "img/trad" + i + ".jpg";
+        imageSources.apartments[i - 1] = "img/ap" + i + ".jpg";
+    }
+
+    const pageName = window.location.pathname.split('/').pop().split('.')[0];
+
+    const pageImages = imageSources[pageName];
+
+
+    const imageGrid = document.querySelector(".image-grid");
+
+    // Loop through image sources and create image containers
+    pageImages.forEach((src, index) => {
+        const imageContainer = document.createElement("div");
+        imageContainer.classList.add("image-container");
+
+        const image = document.createElement("img");
+        image.src = src;
+        image.alt = `Image ${index + 1}`;
+
+        const priceOverlay = document.createElement("div");
+        priceOverlay.classList.add("price-overlay");
+        priceOverlay.textContent = "$_______";
+
+        const button = document.createElement("button");
+        button.textContent = "Contact Seller";
+
+        // Append image, price overlay, and button to the image container
+        imageContainer.appendChild(image);
+        imageContainer.appendChild(priceOverlay);
+        imageContainer.appendChild(button);
+
+        // Append the image container to the image grid
+        imageGrid.appendChild(imageContainer);
+    });
+});
